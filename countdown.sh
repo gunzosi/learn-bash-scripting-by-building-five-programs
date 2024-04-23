9f783301f2a5dc55082b33748c5fb6c8cba25cb6
Dang Dai Duong <oceanwork2820@gmail.com>
2024-04-23 12:26:47 +0000

    Save progress: 550.1

--- countdown.sh
+++ countdown.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # Program that counts down to zero from a given argument
 
-if [[ $1 -lt 5 ]]
+#!/bin/bash
+# Program that counts down to zero from a given argument
+
+if [[ $1 -le 5 ]]
 then
   echo true
 else
   echo false
 fi
-
